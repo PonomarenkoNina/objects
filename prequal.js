38af71994b1e13ce31a143db8656ec7a72cf90c7
PonomarenkoNina <ninulka@ukr.net>
2024-01-08 20:16:39 +0200

    added Object-o-matic

--- prequal.js
+++ prequal.js
@@ -26,7 +26,7 @@ if (worthAlook) {
 }
 // Console JS: You should really pass on the Webville Motors Taxi
 
-// Exersise 1
+// Exersise
 function getSecret(file, secretPassword) {
   file.opened = file.opened + 1;
   if (secretPassword == file.password) {
@@ -55,3 +55,43 @@ console.log(secret);
 setSecret(superSecretFile, 2, "Dr. Evel's next meeting is in Philadelphia.");
 secret = getSecret(superSecretFile, 2);
 console.log(secret);
+
+// Object-o-matic
+function makeCar() {
+  var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
+  var models = ["Cadillsc", "500", "Bel-Air", "Taxi", "Torpedo"];
+  var years = [1955, 1957, 1948, 1954, 1961];
+  var colors = ["red", "blue", "tan", "yellow", "white"];
+  var convertible = [true, false];
+
+  var rand1 = Math.floor(Math.random() * makes.length);
+  var rand2 = Math.floor(Math.random() * models.length);
+  var rand3 = Math.floor(Math.random() * years.length);
+  var rand4 = Math.floor(Math.random() * colors.length);
+  var rand5 = Math.floor(Math.random() * 5) + 1;
+  var rand6 = Math.floor(Math.random() * 2);
+
+  var car = {
+    make: makes[rand1],
+    model: models[rand2],
+    year: years[rand3],
+    color: colors[rand4],
+    passengers: rand5,
+    convertible: convertible[rand6],
+    mileage: 0,
+  };
+  return car;
+}
+
+function displayCar(car) {
+  console.log(
+    "Your new car is a " + car.year + " " + car.make + " " + car.model
+  );
+}
+
+var carToSell = makeCar();
+displayCar(carToSell);
+// Console JS: Your new car is a 1957 Fiat Torpedo
+// Your new car is a 1961 Fiat Bel-Air
+// Your new car is a 1955 Fiat Cadillsc
+// Your new car is a 1954 GM 500 .......
